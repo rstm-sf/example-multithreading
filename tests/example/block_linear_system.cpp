523bffb91a294a346c4b3ef230262571f2df71b3
Rustam Sayfutdinov <rstm.sf@gmail.com>
2018-12-16 22:33:29 +0300

    Добавлен тест

--- tests/example/block_linear_system.cpp
+++ tests/example/block_linear_system.cpp
@@ -31,3 +31,31 @@ TEST_F(BlockLinearSystemTests, simple) {
 
   EXPECT_EQ(bls.nsteps(), 0);
 }
+
+TEST_F(BlockLinearSystemTests, test_default) {
+  std::size_t nblocks {2};
+  std::size_t max_steps{100};
+  float accuracy {1.0e-6};
+  std::size_t nrows {4};
+  std::vector<float> A({
+    10.0, -1.0,  2.0,  0.0,
+    -1.0, 11.0, -1.0,  3.0,
+     2.0, -1.0, 10.0, -1.0,
+     0.0,  3.0, -1.0,  8.0
+  });
+  std::vector<float> rhs({6.0, 25.0, -11.0, 15.0});
+
+  ex_m_thr::BlockLinearSystem bls(nblocks, max_steps, accuracy, nrows, A, rhs);
+
+  std::vector<float> exact_solution({0.83486235, 2.3486238, -0.92405063, 1.7594937});
+
+  bls.solve();
+
+  float dd {0.0};
+  for (std::size_t i = 0; i < exact_solution.size(); ++i) {
+    float d = bls.solution()[i] - exact_solution[i];
+    dd += d * d;
+  }
+
+  EXPECT_TRUE(std::sqrt(dd) < 1.0e-5);
+}
--- tests/example/linear_system.cpp
+++ tests/example/linear_system.cpp
@@ -30,7 +30,7 @@ class LinearSystemTests : public ::testing::Test {};
 
 TEST_F(LinearSystemTests, test_default) {
   ex_m_thr::LinearSystem ls;
-  std::vector<float> exact_solution({1.0, 2.0, 3.0}); 
+  std::vector<float> exact_solution({1.0, 2.0, 3.0});
 
   ls.solve();
 
@@ -41,4 +41,31 @@ TEST_F(LinearSystemTests, test_default) {
   }
 
   EXPECT_TRUE(std::sqrt(dd) < 1.0e-6);
+}
+
+TEST_F(LinearSystemTests, ex) {
+  std::size_t max_steps{100};
+  float accuracy {1.0e-6};
+  std::size_t nrows {4};
+  std::vector<float> A({
+    10.0, -1.0,  0.0,  0.0,
+    -1.0, 11.0,  0.0,  0.0,
+     0.0,  0.0, 10.0, -1.0,
+     0.0,  0.0, -1.0,  8.0
+  });
+  std::vector<float> rhs({6.0, 25.0, -11.0, 15.0});
+
+  ex_m_thr::LinearSystem ls(max_steps, accuracy, nrows, A, rhs);
+
+  std::vector<float> exact_solution({0.83486235, 2.3486238, -0.92405063, 1.7594937});
+
+  ls.solve();
+
+  float dd {0.0};
+  for (std::size_t i = 0; i < exact_solution.size(); ++i) {
+    float d = ls.solution()[i] - exact_solution[i];
+    dd += d * d;
+  }
+
+  EXPECT_TRUE(std::sqrt(dd) < 1.0e-5);
 }
